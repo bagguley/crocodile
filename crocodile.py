bdc6c754466ec772cb07797a0d7f83b87e0db1be
Wayne Bagguley <bagguley@users.noreply.github.com>
2014-02-27 23:05:28 +0000

    Changes!

--- crocodile.py
+++ crocodile.py
@@ -1,47 +1,52 @@
+import sys
 import time
 import pifacedigitalio
 
 pfd = pifacedigitalio.PiFaceDigital()
 
-# Set trigger to 0
-pfd.output_pins[7].value=0
+def init():
+    # Set trigger to 0
+    pfd.output_pins[7].value=0
+    
+    # Allow module to settle
+    time.sleep(0.5)
 
-# Allow module to settle
-time.sleep(0.5)
+def ping():
+    # Send 10us pulse to trigger
+    pfd.output_pins[7].value=1
+    time.sleep(0.00001)
+    pfd.output_pins[7].value=0
+    
+    # Time the echo
+    #print("Time the echo")
+    start=time.time()
+    while pfd.input_pins[0].value==1:
+       if (time.time()-start)>0.2:
+          break
+    start=time.time()
+    
+    #print(pfd.input_pins[0].value)
+    #print("Start")
+    
+    stop=time.time()
+    while pfd.input_pins[0].value==0:
+        if (time.time()-stop)>0.2:
+            break
+    stop=time.time()
+    
+    #print(pfd.input_pins[0].value)
+    #print("Stop")
+    
+    elapsed=stop-start
+    
+    distance=(34290/2) * elapsed
+    
+    if (distance < 400):
+        print("Distance {:.1f}".format(distance))
+    
+    time.sleep(0.05)
 
-# Send 10us pulse to trigger
-pfd.output_pins[7].value=1
-time.sleep(0.00001)
-pfd.output_pins[7].value=0
-
-# Time the echo
-print("Time the echo")
-start=time.time()
-while pfd.input_pins[0].value==0:
-   if (time.time()-start)>1:
-      print("break")
-      break
-start=time.time()
-
-print(pfd.input_pins[0].value)
-print("Start")
-
-stop=time.time()
-while pfd.input_pins[0].value==1:
-   if (time.time()-stop)>1:
-      print("break stop")
-      break
-stop=time.time()
-
-print(pfd.input_pins[0].value)
-print("Stop")
-
-elapsed=stop-start
-
-distance=elapsed*34000
-
-distance=distance/2
-
-print("Distance {:.1f}".format(distance))
-
+init()
+while True:
+    ping()
 pfd.deinit_board()
